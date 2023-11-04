45081e3460d17b0c9b853791878ad90e0ddd0df6
Stanislav Sultanov <sultanov.stas@rambler.ru>
2023-11-04 14:48:55 +0300

    Скрпит для копирования проекта в web

---
+++ copy too web.cmd
@@ -0,0 +1,10 @@
+rmdir /s /q "D:\Dev\Dochub\MyProject_Web\metamodel\metamodel\"
+xcopy "D:\Dev\Dochub\MyProject\Metamodel" "D:\Dev\Dochub\MyProject_Web\metamodel\metamodel\" /S /E
+
+
+rmdir /s /q "D:\Dev\Dochub\MyProject_Web\manifest\manifest"
+xcopy "D:\Dev\Dochub\MyProject\Architecture" "D:\Dev\Dochub\MyProject_Web\manifest\manifest\" /S /E
+
+
+
+
