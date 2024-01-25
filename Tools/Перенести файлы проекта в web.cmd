365d696803bd889185e2fa78e69c0de3faf77748
Stanislav <sultanov.stas@rambler.ru>
2024-01-25 14:05:14 +0300

    Изменена структура описания комопнент и их расширяемых характеристик

--- Tools/Перенести файлы проекта в web.cmd
+++ Tools/Перенести файлы проекта в web.cmd
@@ -1,9 +1,8 @@
-rmdir /s /q "D:\Dev\Dochub\MyProject_Web\metamodel\metamodel\"
-xcopy "D:\Dev\Dochub\MyProject\Metamodel" "D:\Dev\Dochub\MyProject_Web\metamodel\metamodel\" /S /E
+rmdir /s /q "Z:\Dev\Dochub\MyProject_Web\metamodel\metamodel\"
+xcopy "Z:\Dev\Dochub\MyProject\Metamodel" "Z:\Dev\Dochub\MyProject_Web\metamodel\metamodel\" /S /E
 
 
-rmdir /s /q "D:\Dev\Dochub\MyProject_Web\manifest\manifest"
-xcopy "D:\Dev\Dochub\MyProject\Datalake" "D:\Dev\Dochub\MyProject_Web\manifest\manifest\" /S /E
+rmdir /s /q "Z:\Dev\Dochub\MyProject_Web\manifest\manifest"
+xcopy "Z:\Dev\Dochub\MyProject\Datalake" "Z:\Dev\Dochub\MyProject_Web\manifest\manifest\" /S /E
 
-explorer "D:\Dev\Dochub\MyProject_Web"
-
+explorer "Z:\Dev\Dochub\MyProject_Web"
